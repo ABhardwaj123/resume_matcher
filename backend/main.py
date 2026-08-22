8dbcc649f24b07eff97f22be22095b17cd4a0583
ABhardwaj123 <agastya.bhardwajdps@gmail.com>
2026-08-22 20:07:45 +0530

    uploading resume and jd fxn

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import UploadFile , Form
+from parsers.pdf_parser import extract_text_from_pdf
+from parsers.docx_parser import extract_text_from_docs
+from matching.tfidf_matcher import calculate_match_score
+from matching.skills_extractor import extract_skills
+import json
+
 
 app = FastAPI()
 app.add_middleware(
@@ -11,6 +17,11 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+
+with open("data/skills_list.json" , "r") as f:
+    skills_list = json.load(f)
+
+
 @app.get('/')
 def home():
     return {"status": "ok123"}
@@ -18,11 +29,30 @@ def home():
 
 @app.post('/match')
 def upload(resume: UploadFile, jd_text: str = Form(...)):
-    print(resume.filename)
-    print(len(jd_text))
+
+    extracted_text = ""
+
+    if resume.filename.endswith('.pdf'):
+        extracted_text = extract_text_from_pdf(resume.file)
+    elif resume.filename.endswith(".docx"):
+        extracted_text = extract_text_from_docs(resume.file)
+    else:
+        return {"error": "invalid resume format"}
+
+    score = calculate_match_score(extracted_text, jd_text)
+
+    resume_skills = extract_skills(extracted_text , skills_list)
+    jd_skills = extract_skills(jd_text , skills_list)
+
+    resume_set = set(resume_skills)
+    jd_set = set(jd_skills)
+
+    matched_skills = resume_set & jd_set
+    missing_skills = jd_set - resume_set
+
 
     return {
-        "score": 72,
-        "matchedSkills": ["python", "sql"],
-        "missingSkills": ["docker", "kubernetes"]
+        "score": score,
+        "matchedSkills": list(matched_skills),
+        "missingSkills": list(missing_skills)
     }
